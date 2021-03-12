3b10957576b987448ad16a434db9bb14a8f6e8b0
Maxine Wu <maxine.wu00@gmail.com>
2021-03-12 10:14:26 -0800

    Added lab 2 files.

---
+++ lab2_ex1.asm
@@ -0,0 +1,26 @@
+;=================================================
+; Name: Maxine Wu
+; Email: mwu031@ucr.edu
+; 
+; Lab: lab 2. ex 1
+; Lab section: 25
+; TA: Enoch Lin
+; 
+;=================================================
+
+.ORIG x3000
+;-------------
+;Instructions
+;-------------
+
+LD R3, DEC_65
+LD R4, HEX_41
+
+HALT
+;-------------
+;Data
+;-------------
+DEC_65	.FILL	#65
+HEX_41	.FILL	x0041
+
+.END
